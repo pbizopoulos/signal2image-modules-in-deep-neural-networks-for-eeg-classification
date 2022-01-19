cebd60e6853da0789d3c7a34364e913681384994
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-01-19 09:25:30 +0200

    update docs

--- docs/main.py
+++ docs/main.py
@@ -8,12 +8,6 @@ def main():
             'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification-tfjs/master/resnet18-1D/model.json',
             'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification-tfjs/master/resnet34-1D/model.json',
             'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification-tfjs/master/resnet50-1D/model.json']
-    model_names = [
-            'alexnet-1D',
-            'lenet-1D',
-            'resnet18-1D',
-            'resnet34-1D',
-            'resnet50-1D']
     class_names = ['Open', 'Closed', 'Healthy', 'Tumor', 'Epilepsy']
     title = 'EEG signal classification demo'
     description = 'NOT FOR MEDICAL USE. Choose a EEG csv file (.txt,.csv) and classify epilepsy using a DNN.'
@@ -21,7 +15,7 @@ def main():
     block_width = 256
     block_height = 256
     input_filename = 'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification/master/docs/example-signal.txt'
-    generate_page_signal_classification(model_dirs, model_names, class_names, title, description, url, block_width, block_height, input_filename)
+    generate_page_signal_classification(model_dirs, class_names, title, description, url, block_width, block_height, input_filename)
 
 
 if __name__ == '__main__':
