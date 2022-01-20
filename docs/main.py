047715e4785d7951898844b0228ac8e39970b44a
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2022-01-20 23:49:06 +0200

    fix docs

--- docs/main.py
+++ docs/main.py
@@ -10,13 +10,13 @@ def main():
             'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification-tfjs/master/resnet34-1D/model.json',
             'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification-tfjs/master/resnet50-1D/model.json']
     class_names = ['Open', 'Closed', 'Healthy', 'Tumor', 'Epilepsy']
+    input_filename = 'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification/master/docs/example-signal-1.txt'
     title = 'EEG signal classification demo'
     description = 'NOT FOR MEDICAL USE. Choose a EEG csv file (.txt,.csv) and classify epilepsy using a DNN.'
     url = 'https://github.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification'
     block_width = 256
     block_height = 256
-    input_filename = 'https://raw.githubusercontent.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification/master/docs/example-signal.txt'
-    generate_page(ml_type, model_dirs, class_names, title, description, url, block_width, block_height, input_filename)
+    generate_page(ml_type, model_dirs, class_names, input_filename, title, description, url, block_width, block_height)
 
 
 if __name__ == '__main__':
