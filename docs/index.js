ac387f0a20a5925d6616f004e4adbe8d42796354
Paschalis Bizopoulos <pbizopoulos@protonmail.com>
2023-08-18 12:34:07 +0300

    .

--- docs/index.js
+++ docs/index.js
@@ -1,19 +1,21 @@
 "use strict";
 const fs = require("fs");
-const handler = require("serve-handler");
 const https = require("https");
-
-const options = {
-	key: fs.readFileSync("tmp/key.pem"),
-	cert: fs.readFileSync("tmp/cert.pem"),
-};
-
-const server = https.createServer(options, (request, response) => {
-	return handler(request, response);
-});
-
+const serveHandler = require("serve-handler");
+const server = https.createServer(
+	{
+		cert: fs.readFileSync("tmp/fullchain.pem"),
+		key: fs.readFileSync("tmp/privkey.pem"),
+	},
+	(request, response) => {
+		return serveHandler(request, response);
+	},
+);
 if (process.env.DEBUG !== "1") {
 	server.listen(8000, "172.17.0.2", () => {
 		console.log("Running at https://172.17.0.2:8000");
+		process.on("SIGINT", () => {
+			process.exit(0);
+		});
 	});
 }
