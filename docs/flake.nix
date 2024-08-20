0b8d2f628156f2c216b439664e000844a94636ae
Paschalis Bizopoulos <pbizop@gmail.com>
2024-08-20 21:00:17 +0300

    .

--- docs/flake.nix
+++ docs/flake.nix
@@ -1,53 +1,60 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
   };
 
   outputs =
     {
       self,
       nixpkgs,
-      flake-utils,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        packagesAll = [
-          pkgs.http-server
-          pkgs.openssl
-        ];
-      in
-      {
-        devShells.all = pkgs.mkShell {
-          buildInputs = packagesAll;
-          shellHook = ''
-            [ ! -z $STAGE ] && openssl req -subj '/C=..' -nodes -x509 -keyout tmp/privkey.pem -out tmp/fullchain.pem && http-server --tls --cert tmp/fullchain.pem --key tmp/privkey.pem || true
-            exit
-          '';
-        };
-        devShells.check = pkgs.mkShell {
-          buildInputs = packagesAll ++ [
-            pkgs.biome
-            pkgs.git
-            pkgs.nixfmt-rfc-style
-            pkgs.nodePackages.js-beautify
-            pkgs.nodejs
+      flake-parts,
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs { inherit system; };
+          packagesAll = [
+            pkgs.http-server
+            pkgs.openssl
           ];
-          shellHook = ''
-            set -e
-            nix flake check
-            nix fmt
-            js-beautify --end-with-newline --indent-inner-html --no-preserve-newlines --type html --replace index.html
-            [ -e script.js ] && biome check --unsafe --write script.js || true
-            ls -ap | grep -v -E -x './|../|.env|.gitignore|CNAME|Makefile|index.html|flake.lock|flake.nix|prm/|pyscript/|python/|script.js|style.css|tmp/' | grep -q . && exit 1 || true
-            test $(basename $(pwd)) = 'docs'
-            exit
-          '';
+        in
+        {
+          devShells.all = pkgs.mkShell {
+            buildInputs = packagesAll;
+            shellHook = ''
+              [ ! -z $STAGE ] && openssl req -subj '/C=..' -nodes -x509 -keyout tmp/privkey.pem -out tmp/fullchain.pem && http-server --tls --cert tmp/fullchain.pem --key tmp/privkey.pem || true
+              exit
+            '';
+          };
+          devShells.check = pkgs.mkShell {
+            buildInputs = packagesAll ++ [
+              pkgs.biome
+              pkgs.git
+              pkgs.nixfmt-rfc-style
+              pkgs.nodePackages.js-beautify
+              pkgs.nodejs
+            ];
+            shellHook = ''
+              set -e
+              nix flake check
+              nix fmt
+              js-beautify --end-with-newline --indent-inner-html --no-preserve-newlines --type html --replace index.html
+              [ -e script.js ] && biome check --unsafe --write script.js || true
+              ls -ap | grep -v -E -x './|../|.env|.gitignore|CNAME|Makefile|index.html|flake.lock|flake.nix|prm/|pyscript/|python/|script.js|style.css|tmp/' | grep -q . && exit 1 || true
+              test $(basename $(pwd)) = 'docs'
+              exit
+            '';
+          };
+          devShells.default = pkgs.mkShell { buildInputs = packagesAll; };
+          formatter = pkgs.nixfmt-rfc-style;
         };
-        devShells.default = pkgs.mkShell { buildInputs = packagesAll; };
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+    };
 }
--- latex/python/flake.nix
+++ latex/python/flake.nix
@@ -1,7 +1,7 @@
 {
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-24.05";
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
     check-python-script.url = "github:pbizopoulos/check-python-script?dir=python";
     onnxscript.url = "github:pbizopoulos/nixpkgs?dir=onnxscript";
   };
@@ -10,64 +10,71 @@
     {
       self,
       nixpkgs,
-      flake-utils,
+      flake-parts,
       check-python-script,
       onnxscript,
-    }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs {
-          inherit system;
-          config.allowUnfree = true;
-        };
-        packagesAll = [
-          onnxscript.packages.${system}.default
-          pkgs.python3Packages.matplotlib
-          pkgs.python3Packages.onnx
-          pkgs.python3Packages.pandas
-          pkgs.python3Packages.scipy
-          pkgs.python3Packages.torch-bin
-          pkgs.python3Packages.torchvision-bin
-          pkgs.python3Packages.types-requests
-        ];
-      in
-      {
-        devShells.all = pkgs.mkShell {
-          PYTHONDONTWRITEBYTECODE = true;
-          buildInputs = packagesAll;
-          shellHook = ''
-            set -e
-            python3 main.py || exit
-            exit
-          '';
-        };
-        devShells.check = pkgs.mkShell {
-          buildInputs = packagesAll ++ [
-            check-python-script.packages.${system}.default
-            pkgs.djlint
-            pkgs.git
-            pkgs.mypy
-            pkgs.nixfmt-rfc-style
-            pkgs.ruff
+    }@inputs:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      perSystem =
+        { system, ... }:
+        let
+          pkgs = import nixpkgs {
+            inherit system;
+            config.allowUnfree = true;
+          };
+          packagesAll = [
+            onnxscript.packages.${system}.default
+            pkgs.python3Packages.matplotlib
+            pkgs.python3Packages.onnx
+            pkgs.python3Packages.pandas
+            pkgs.python3Packages.scipy
+            pkgs.python3Packages.torch-bin
+            pkgs.python3Packages.torchvision-bin
+            pkgs.python3Packages.types-requests
           ];
-          shellHook = ''
-            set -e
-            nix flake check
-            nix fmt
-            check-python-script main.py
-            ruff format --cache-dir tmp/ruff main.py
-            ruff check --cache-dir tmp/ruff --exit-non-zero-on-fix --fix --select ALL --unsafe-fixes main.py
-            mypy --cache-dir tmp/mypy --ignore-missing-imports --strict main.py
-            if [ -d 'templates/' ]; then djlint templates/ --lint --profile=jinja --quiet --reformat; fi
-            [ -z $STAGE ] || (unset STAGE && pydoc -w main && mv main.html tmp/)
-            ls -ap | grep -v -E -x './|../|.env|.gitignore|Makefile|flake.lock|flake.nix|main.py|prm/|pyproject.toml|python/|result|static/|templates/|tmp/' | grep -q . && exit 1 || true
-            test $(basename $(pwd)) = 'python'
-            exit
-          '';
+        in
+        {
+          devShells.all = pkgs.mkShell {
+            PYTHONDONTWRITEBYTECODE = true;
+            buildInputs = packagesAll;
+            shellHook = ''
+              set -e
+              python3 main.py || exit
+              exit
+            '';
+          };
+          devShells.check = pkgs.mkShell {
+            buildInputs = packagesAll ++ [
+              check-python-script.packages.${system}.default
+              pkgs.djlint
+              pkgs.git
+              pkgs.mypy
+              pkgs.nixfmt-rfc-style
+              pkgs.ruff
+            ];
+            shellHook = ''
+              set -e
+              nix flake check
+              nix fmt
+              check-python-script main.py
+              ruff format --cache-dir tmp/ruff main.py
+              ruff check --cache-dir tmp/ruff --exit-non-zero-on-fix --fix --select ALL --unsafe-fixes main.py
+              mypy --cache-dir tmp/mypy --ignore-missing-imports --strict main.py
+              if [ -d 'templates/' ]; then djlint templates/ --lint --profile=jinja --quiet --reformat; fi
+              [ -z $STAGE ] || (unset STAGE && pydoc -w main && mv main.html tmp/)
+              ls -ap | grep -v -E -x './|../|.env|.gitignore|Makefile|flake.lock|flake.nix|main.py|prm/|pyproject.toml|python/|result|static/|templates/|tmp/' | grep -q . && exit 1 || true
+              test $(basename $(pwd)) = 'python'
+              exit
+            '';
+          };
+          devShells.default = pkgs.mkShell { buildInputs = packagesAll; };
+          formatter = pkgs.nixfmt-rfc-style;
         };
-        devShells.default = pkgs.mkShell { buildInputs = packagesAll; };
-        formatter = pkgs.nixfmt-rfc-style;
-      }
-    );
+    };
 }
