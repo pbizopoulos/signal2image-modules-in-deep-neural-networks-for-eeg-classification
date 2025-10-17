4dd450d6d155c38def21ab9722df53b5f919ae49
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-17 12:36:43 +0300

    .

--- packages/default/default.nix
+++ packages/default/default.nix
@@ -2,16 +2,36 @@
   inputs,
   pkgs ? import <nixpkgs> { },
 }:
-pkgs.writeShellApplication {
-  name = builtins.baseNameOf ./.;
-  runtimeInputs = [
-    inputs.self.packages.${pkgs.stdenv.system}.python
+let
+  pythonEnv = pkgs.python312.withPackages (_ps: [
+    inputs.self.packages.${pkgs.stdenv.system}.onnxscript
+    pkgs.python312Packages.matplotlib
+    pkgs.python312Packages.pandas
+    pkgs.python312Packages.scipy
+    pkgs.python312Packages.torch-bin
+    pkgs.python312Packages.torchvision-bin
+  ]);
+in
+pkgs.stdenv.mkDerivation rec {
+  buildInputs = [
     pkgs.texlive.combined.scheme-full
+    pythonEnv
   ];
-  text = ''
-    python
-    cd ${../latex}
-    latexmk -outdir="$HOME"/github.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification/packages/latex/tmp/ -pdf ms.tex
-    xdg-open "$HOME"/github.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification/packages/latex/tmp/ms.pdf
+  installPhase = ''
+    mkdir -p $out/bin
+    echo '#!/usr/bin/env bash
+      set -e
+      package_dir=$HOME/github.com/pbizopoulos/signal2image-modules-in-deep-neural-networks-for-eeg-classification/packages/default
+      tmp_dir=$(mktemp -d)
+      cp -r ${src}/* "$tmp_dir"
+      cd "$tmp_dir"
+      ${pythonEnv}/bin/python ./main.py
+      ${pkgs.texlive.combined.scheme-full}/bin/latexmk -outdir=$package_dir/tmp -pdf ./ms.tex
+      ' > $out/bin/${pname}
+    chmod +x $out/bin/${pname}
   '';
+  meta.mainProgram = pname;
+  pname = builtins.baseNameOf src;
+  src = ./.;
+  version = "0.0.0";
 }
