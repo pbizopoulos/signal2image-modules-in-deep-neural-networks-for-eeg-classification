a1cb61346062419c8e82c0a2f167543071f5b4f7
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-22 14:35:17 +0300

    .

--- checks/default/default.nix
+++ checks/default/default.nix
@@ -3,7 +3,6 @@ pkgs.testers.runNixOSTest rec {
   name = builtins.baseNameOf ./.;
   nodes.machine = {
     environment.systemPackages = [ inputs.self.packages.${pkgs.stdenv.system}.${name} ];
-    networking.useDHCP = true;
     virtualisation.memorySize = 8192;
   };
   testScript = ''machine.succeed("DEBUG=1 ${name}")'';
