74f108b171850e50832e0cd8b147b05cde15ecc9
Paschalis Bizopoulos <pbizop@gmail.com>
2025-10-22 15:49:11 +0300

    .

--- formatter.nix
+++ formatter.nix
@@ -1,60 +1,74 @@
-{ inputs, pkgs, ... }:
-inputs.treefmt-nix.lib.mkWrapper pkgs {
-  programs = {
-    actionlint.enable = true;
-    beautysh.enable = true;
-    biome.enable = true;
-    deadnix.enable = true;
-    nixfmt = {
-      enable = true;
-      strict = true;
+{
+  flake,
+  inputs,
+  pkgs,
+  ...
+}:
+let
+  formatter = treefmtEval.config.build.wrapper;
+  treefmtEval = inputs.treefmt-nix.lib.evalModule pkgs {
+    programs = {
+      actionlint.enable = true;
+      beautysh.enable = true;
+      biome.enable = true;
+      deadnix.enable = true;
+      nixfmt = {
+        enable = true;
+        strict = true;
+      };
+      prettier.enable = true;
+      ruff-check = {
+        enable = true;
+        extendSelect = [ "ALL" ];
+      };
+      ruff-format.enable = true;
+      shellcheck.enable = true;
+      shfmt = {
+        enable = true;
+        simplify = true;
+      };
+      statix.enable = true;
+      texfmt.enable = true;
+      yamlfmt.enable = true;
     };
-    prettier.enable = true;
-    ruff-check = {
-      enable = true;
-      extendSelect = [ "ALL" ];
+    projectRootFile = "flake.nix";
+    settings = {
+      formatter = {
+        bibtex-tidy = {
+          command = pkgs.bibtex-tidy;
+          includes = [ "*.bib" ];
+          options = [
+            "--duplicates"
+            "--no-align"
+            "--no-wrap"
+            "--sort"
+            "--sort-fields"
+            "--v2"
+          ];
+        };
+        mypy = {
+          command = pkgs.mypy;
+          includes = [ "*.py" ];
+          options = [
+            "--explicit-package-bases"
+            "--ignore-missing-imports"
+            "--strict"
+          ];
+        };
+        ruff-check.options = [ "--unsafe-fixes" ];
+        shfmt.options = [ "--posix" ];
+        texfmt.options = [ "--nowrap" ];
+      };
+      global.excludes = [
+        "*/prm/**"
+        "*/tmp/**"
+      ];
     };
-    ruff-format.enable = true;
-    shellcheck.enable = true;
-    shfmt = {
-      enable = true;
-      simplify = true;
-    };
-    statix.enable = true;
-    texfmt.enable = true;
-    yamlfmt.enable = true;
   };
-  projectRootFile = "flake.nix";
-  settings = {
-    formatter = {
-      bibtex-tidy = {
-        command = pkgs.bibtex-tidy;
-        includes = [ "*.bib" ];
-        options = [
-          "--duplicates"
-          "--no-align"
-          "--no-wrap"
-          "--sort"
-          "--sort-fields"
-          "--v2"
-        ];
-      };
-      mypy = {
-        command = pkgs.mypy;
-        includes = [ "*.py" ];
-        options = [
-          "--explicit-package-bases"
-          "--ignore-missing-imports"
-          "--strict"
-        ];
-      };
-      ruff-check.options = [ "--unsafe-fixes" ];
-      shfmt.options = [ "--posix" ];
-      texfmt.options = [ "--nowrap" ];
-    };
-    global.excludes = [
-      "*/prm/**"
-      "*/tmp/**"
-    ];
+in
+formatter
+// {
+  passthru = formatter.passthru // {
+    tests.check = treefmtEval.config.build.check flake;
   };
 }
